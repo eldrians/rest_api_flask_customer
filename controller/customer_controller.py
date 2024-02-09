4419bb4806d3db60e2c924c38fe121ec7d304060
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2024-02-09 19:04:02 +0700

    app(): create basic CRUD for customer

--- controller/customer_controller.py
+++ controller/customer_controller.py
@@ -1,52 +1,52 @@
 from app import app
-from model.user_model import user_model
-from model.auth_model import auth_model
+from model.customer_model import customer_model
+# from model.auth_model import auth_model
 from flask import request, send_file
 from datetime import datetime
 
 obj = customer_model()
-auth = auth_model()
+# auth = auth_model()
 
 @app.route("/customer")
-@auth.token_auth()
-def user_get_all_controller():
-    return obj.user_get_all_model()
+# @auth.token_auth()
+def customer_get_all_controller():
+    return obj.customer_get_all_model()
 
 @app.route("/user/add", methods=["POST"])
-@auth.token_auth()
-def user_add_controller():
-    return obj.user_add_model(request.form)
+# @auth.token_auth()
+def costumer_add_controller():
+    return obj.costumer_add_model(request.form)
 
 @app.route("/user/update", methods=["PUT"])
-def user_update_controller():
-    return obj.user_update_model(request.form)
+def costumer_update_controller():
+    return obj.costumer_update_model(request.form)
 
 @app.route("/user/delete/<id>", methods=["DELETE"])
-def user_delete_controller(id):
-    return obj.user_delete_model(id)
+def costumer_delete_controller(id):
+    return obj.costumer_delete_model(id)
 
 @app.route("/user/patch/<id>", methods=["PATCH"])
-def user_patch_controller(id):
-    return obj.user_patch_model(request.form, id)
+def costumer_patch_controller(id):
+    return obj.costumer_patch_model(request.form, id)
 
-@app.route("/user/all/limit/<limit>/page/<page>", methods=["GET"])
-def user_pagination_controller(limit, page):
-    return obj.user_pagination_model(limit, page)
+# @app.route("/user/all/limit/<limit>/page/<page>", methods=["GET"])
+# def user_pagination_controller(limit, page):
+#     return obj.user_pagination_model(limit, page)
 
-@app.route("/user/<uid>/upload/avatar", methods=["PUT"])
-def user_upload_avatar_controller(uid):
-    file = request.files['avatar']
-    uniqueFileName = str(datetime.now().timestamp()).replace(".","")
-    fileNameSplit = file.filename.split(".")
-    ext = fileNameSplit[len(fileNameSplit)-1]
-    finalFilePath = f"uploads/{uniqueFileName}.{ext}" 
-    file.save(finalFilePath)
-    return obj.user_upload_avatar_model(uid, finalFilePath)
+# @app.route("/user/<uid>/upload/avatar", methods=["PUT"])
+# def user_upload_avatar_controller(uid):
+#     file = request.files['avatar']
+#     uniqueFileName = str(datetime.now().timestamp()).replace(".","")
+#     fileNameSplit = file.filename.split(".")
+#     ext = fileNameSplit[len(fileNameSplit)-1]
+#     finalFilePath = f"uploads/{uniqueFileName}.{ext}" 
+#     file.save(finalFilePath)
+#     return obj.user_upload_avatar_model(uid, finalFilePath)
 
-@app.route("/uploads/<filename>")
-def user_getavatar_controller(filename):
-    return send_file(f"uploads/{filename}")
+# @app.route("/uploads/<filename>")
+# def user_getavatar_controller(filename):
+#     return send_file(f"uploads/{filename}")
 
-@app.route("/user/login", methods=["POST"])
-def user_login_controller():
-    return obj.user_login_model(request.form)
+# @app.route("/user/login", methods=["POST"])
+# def user_login_controller():
+#     return obj.user_login_model(request.form)
--- model/customer_model.py
+++ model/customer_model.py
@@ -2,10 +2,10 @@ import mysql.connector
 import json
 from flask import make_response
 from datetime import datetime, timedelta
-import jwt
+# import jwt
 from config.config import dbconfig
 
-class user_model():
+class customer_model():
     def __init__(self):
         try:
             self.con=mysql.connector.connect(host=dbconfig['hostname'], user=dbconfig['username'], password=dbconfig['password'], database=dbconfig['database'])
@@ -15,7 +15,7 @@ class user_model():
         except: 
             print('error')
 
-    def user_get_all_model(self):
+    def customer_get_all_model(self):
         self.cur.execute("SELECT * FROM users")
         result = self.cur.fetchall()
         if len(result)>0:
@@ -25,14 +25,14 @@ class user_model():
         else:
             return make_response({"message": "No data found"}, 204)
         
-    def user_add_model(self, data):
+    def customer_add_model(self, data):
         query = "INSERT INTO users(name, email, phone, role, password) VALUES (%s, %s, %s, %s, %s)"
         values = (data['name'], data['email'], data['phone'], data['role'], data['password'])
 
         self.cur.execute(query, values)
         return make_response({"message": "User successfully created"}, 201)
     
-    def user_update_model(self, data):
+    def customer_update_model(self, data):
         query = "UPDATE users SET name=%s, email=%s, phone=%s, role=%s, password=%s WHERE id=%s"
         values = (data['name'], data['email'], data['phone'], data['role'], data['password'], data['id'])
 
@@ -42,14 +42,14 @@ class user_model():
         else:
             return make_response({"message": "Nothing to update"}, 202)
         
-    def user_delete_model(self, id):
+    def customer_delete_model(self, id):
         self.cur.execute(f"DELETE FROM users WHERE id={id}")
         if self.cur.rowcount>0:
             return make_response({"message": "User deleted successfully"}, 200)
         else:
             return make_response({"message": "Nothing to delete"}, 202)
     
-    def user_patch_model(self, data, id):
+    def customer_patch_model(self, data, id):
         query="UPDATE users SET "
         for key in data:
             query += f"{key}='{data[key]}',"
@@ -62,40 +62,40 @@ class user_model():
         else:
             return make_response({"message": "Nothing to update"}, 202)
         
-    def user_pagination_model(self, limit, page):
-        limit = int(limit)
-        page = int(page)
-        start = (page*limit)-limit
+    # def user_pagination_model(self, limit, page):
+    #     limit = int(limit)
+    #     page = int(page)
+    #     start = (page*limit)-limit
 
-        query = f"SELECT * FROM users LIMIT {start}, {limit}"
-        self.cur.execute(query)
-        result = self.cur.fetchall()
-        if len(result)>0:
-            res = make_response({"payload":result ,"page_no":page ,"limit":limit}, 200)
-            return res
-        else:
-            return make_response({"message": "No data found"}, 204)
+    #     query = f"SELECT * FROM users LIMIT {start}, {limit}"
+    #     self.cur.execute(query)
+    #     result = self.cur.fetchall()
+    #     if len(result)>0:
+    #         res = make_response({"payload":result ,"page_no":page ,"limit":limit}, 200)
+    #         return res
+    #     else:
+    #         return make_response({"message": "No data found"}, 204)
     
-    def user_upload_avatar_model(self, uid, finalpath):
-        query = "UPDATE users SET avatar=%s WHERE id=%s"
-        values = (finalpath, uid)
+    # def user_upload_avatar_model(self, uid, finalpath):
+    #     query = "UPDATE users SET avatar=%s WHERE id=%s"
+    #     values = (finalpath, uid)
 
-        self.cur.execute(query, values)
-        if self.cur.rowcount>0:
-            return make_response({"message": "FILE_UPLOADED_SUCCESSFULLY"}, 201)
-        else:
-            return make_response({"message": "Nothing to update"}, 202)
+    #     self.cur.execute(query, values)
+    #     if self.cur.rowcount>0:
+    #         return make_response({"message": "FILE_UPLOADED_SUCCESSFULLY"}, 201)
+    #     else:
+    #         return make_response({"message": "Nothing to update"}, 202)
         
-    def user_login_model(self, data):
-        # print(str(data))
-        self.cur.execute(f"SELECT id, name, email, phone, avatar, role_id FROM users WHERE email='{data['email']}' and password='{data['password']}'")
-        result = self.cur.fetchall()
-        userdata = result[0]
-        exp_time = datetime.now() + timedelta(minutes=15)
-        exp_epoch_time = int(exp_time.timestamp())
-        payload = {
-            "payload": userdata,
-            "exp": exp_epoch_time
-        }
-        jwtoken = jwt.encode(payload, "sagar", algorithm="HS256")
-        return make_response({"token":jwtoken}, 200)
+    # def user_login_model(self, data):
+    #     # print(str(data))
+    #     self.cur.execute(f"SELECT id, name, email, phone, avatar, role_id FROM users WHERE email='{data['email']}' and password='{data['password']}'")
+    #     result = self.cur.fetchall()
+    #     userdata = result[0]
+    #     exp_time = datetime.now() + timedelta(minutes=15)
+    #     exp_epoch_time = int(exp_time.timestamp())
+    #     payload = {
+    #         "payload": userdata,
+    #         "exp": exp_epoch_time
+    #     }
+    #     jwtoken = jwt.encode(payload, "sagar", algorithm="HS256")
+    #     return make_response({"token":jwtoken}, 200)
